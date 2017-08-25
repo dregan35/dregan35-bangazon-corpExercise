5156fd64eff1e148e1fe37cc9ca4fbbce92a5f7f
dregan35 <dregan1@comcast.net>
2017-08-25 14:27:44 -0500

    loaded txt file synchronously

---
+++ file-io/file-io.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const readable = require("fs");
+
+try {
+  var data = readable.readFileSync("input-file.txt", "utf8");
+  console.log(data);
+} catch (err) {
+  console.log("err", e.stack);
+}
